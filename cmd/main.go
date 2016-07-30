dd26e506a09ca29d9d92fc9082545e4c94a23fc5
Glebov Boris <bglebov@ussc.ru>
2016-07-30 22:59:45 +0500

    Firstly run bot was successfully.

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/avatar29A/mellody-bot"
+	"os"
+)
+
+const (
+	TELEGRAM_BOT_API_KEY = "TELEGRAM_BOT_API_KEY"
+)
 
 func main() {
+	bot := mellody_bot.NewTelegramBot(os.Getenv(TELEGRAM_BOT_API_KEY))
+	bot.Start()
 	fmt.Print("Hello hakatone!")
 }
